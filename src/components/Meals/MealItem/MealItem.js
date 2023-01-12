3f19e9e38a201d0b63e69a82946eee33dfd64050
Nanok24 <ionmihneaandrei@gmail.com>
2023-01-12 21:33:46 +0200

    Updated the cart modal with state & implemented a cart context and reducer

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,24 @@
+import { useContext } from "react";
+
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/CartContext";
 
 const MealItem = (props) => {
+  const cartCtx = useContext(CartContext);
+
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    console.log("AddToCartHandler is called");
+    console.log("Amount from addToCartHandler: ", amount);
+    cartCtx.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
 
   return (
     <li className={classes.meal}>
@@ -12,7 +28,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm id={props.id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,15 @@
+import React from "react";
+
 import classes from "./Input.module.css";
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
       {/* Using the spread operator I make sure that any other key value pair I receive in this input object are added as props to input. For eg: if I have {type: 'text'}, the spread operator adds the type prop to the input*/}
     </div>
   );
-};
+});
 
 export default Input;
